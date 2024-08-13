86d4eccf7053727a5725ce8cafe051de002a90f1
abmohiz <abmohiz22@gmail.com>
2024-08-13 18:30:45 +0500

    created another class for prinitng keys.

--- OOP_Practices/flashcard.py
+++ OOP_Practices/flashcard.py
@@ -1,3 +1,4 @@
+import random
 class FlashCard():
 
     def __init__(self, word, meaning):
@@ -11,18 +12,45 @@ class FlashCard():
 
 flash = []
 
-while(True):
-    word = input("enter word")
-    meaning = input("enter the meaning ")
+# while(True):
+#     word = input("enter word")
+#     meaning = input("enter the meaning ")
 
-    flash.append(FlashCard(word, meaning))
-    option = int(input("Enter 0 to add another"))
-    if option == 0:
-        continue
-    else:
-        break
+    # flash.append(FlashCard(word, meaning))
+#     option = int(input("Enter 0 to add another"))
+#     if option == 0:
+#         continue
+#     else:
+#         break
+#
+# print("Your Flashcards")
+#
+# for i in flash:
+#     print(">", i)
 
-print("Your Flashcards")
 
-for i in flash:
-    print(">", i)
+class FlashCard1():
+
+
+
+    def __init__(self, fruits):
+        self.fruits = fruits
+
+    def quiz(self):
+
+        for fruit in self.fruits.keys():
+            print(fruit)
+
+fruits = {'apple': "red",
+                       'banana': "yellow",
+                       'watermelon': 'green',
+                       'grapes': 'purple',
+                       'mango':"yellow"}
+Keys = FlashCard1(fruits=fruits)
+Keys.quiz()
+
+
+
+
+
+
