8ee7269099727e3b88379cd070ae7e8bba631493
abmohiz <abmohiz22@gmail.com>
2024-08-21 07:40:46 +0500

    Added function for counting right answers and making points for the quiz.

--- OOP_Practices/flashcard.py
+++ OOP_Practices/flashcard.py
@@ -1,4 +1,5 @@
 import random
+
 class FlashCard():
 
     def __init__(self, word, meaning):
@@ -35,23 +36,35 @@ class FlashCard1():
 
     def __init__(self, fruits):
         self.fruits = fruits
+        self.right_answers = 0
+
 
     def quiz(self):
+        print("Options are :",fruits.values())
 
+        self.right_answers = 0
         for fruit, color in fruits.items():
+
             print(fruit)
             ask = input("Enter the name of fruit color")
             print(ask)
             if ask == color:
                 if True:
                     print("True")
+                    self.right_answers +=1
                     continue
 
-            if ask == len(fruits):
-                print("all correct full marks")
+
             else:
                 print("Wrong answer")
                 break
+
+        print(f"Total right answers are: {self.right_answers}")
+
+
+    def calculate_points(self):
+        total_points = self.right_answers * 5
+        print("Total points are ",total_points)
 
 
 
@@ -64,8 +77,7 @@ fruits = {'apple': "red",
                        'mango':"yellow"}
 Keys = FlashCard1(fruits=fruits)
 Keys.quiz()
-
-
+Keys.calculate_points()
 
 
 
