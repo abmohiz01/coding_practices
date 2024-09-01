b7bcdbbf5f410468d7389b0c4511796a7226ff1a
abmohiz <abmohiz22@gmail.com>
2024-09-01 20:07:27 +0500

    two methods for removing duplicates from a string.

--- Interview_Questions/String_Questions.py
+++ Interview_Questions/String_Questions.py
@@ -1,7 +1,7 @@
 """Reverse a string """
 
 def reverse(word :str) -> str:
-    print(word[::-1])
+    return word[::-1]
 
 def reverse_string(word_2: str) -> str:
     myword = ""
@@ -13,3 +13,48 @@ def reverse_string(word_2: str) -> str:
 
 #reverse("hello world")
 print(reverse_string("helloworld 2"))
+
+'''Write a Python function to check if a string is a palindrome'''
+
+def palandrome(palind_word: str) -> bool:
+    '''It first reverse a strings and then check if it is true or not'''
+    word_reversing = reverse(palind_word)
+
+    if word_reversing == palind_word:
+        print("PALINDROME ")
+        return True
+
+    else:
+        print("NOT PALINDROME ")
+        return False
+
+
+print(palandrome("nayak"))
+
+def remove_duplicates(word3):
+    """Using a Set method"""
+
+    my_list = []
+    unique_word = set()
+    for word in word3:
+       if word not in unique_word:
+           unique_word.add(word)
+           my_list.append(word)
+
+    print("Unique characters in order:", "".join(my_list))
+
+def remove_char(word4):
+    """Removing duplicates in string using a list """
+
+    list_4 = []
+    for char in word4:
+        if char not in list_4:
+            list_4.append(char)
+
+
+    print("Removed duplicates in string using list method :", "".join(list_4))
+
+
+
+remove_duplicates("hello")
+remove_char("Programming")
