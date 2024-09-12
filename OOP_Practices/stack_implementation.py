6a362d48b9b909a9d47801730edc5a947fbddd0a
abmohiz <abmohiz22@gmail.com>
2024-09-12 13:20:25 +0500

    Added class to implement deque for stack implementation.

--- OOP_Practices/stack_implementation.py
+++ OOP_Practices/stack_implementation.py
@@ -25,7 +25,35 @@ class StackImplementation_List():
         print(self.my_list_stack)
 
 
-demo_list = [i for i in range(1,20+1)]
+demo_list= [i for i in range(1,20+1)]
 mystact =StackImplementation_List(my_list_stack=demo_list)
 mystact.pushing_elements()
 mystact.poping_elements()
+
+class StackImplementaion_deques():
+
+    def __init__(self,my_deques: deque):
+
+        self.mydeques = my_deques
+
+    def pushing_deque(self):
+
+        self.mydeques = []
+        for deq in demo_list:
+            self.mydeques.append(deq)
+            print(f"Pushing using deque:{deq}")
+
+        print(self.mydeques)
+
+    def poping_deque(self):
+
+        while self.mydeques:
+            popped_queue = self.mydeques.pop()
+            print("Poping :",popped_queue)
+
+        print(self.mydeques)
+
+demo_list1= [i for i in range(1,20+1)]
+demo_deq = StackImplementaion_deques(my_deques=demo_list1)
+demo_deq.pushing_deque()
+demo_deq.poping_deque()
